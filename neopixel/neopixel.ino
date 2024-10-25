0393daf7d4bb8e34baec2ec6ee659cefd8b52382
Kobe Yang <kobeyang160@gmail.com>
2024-10-25 12:41:42 -0700

    Added power define

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -1,21 +1,31 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN        6
-#define NUMPIXELS  8
-#define DELAYVAL   500
-
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN       6
+#define NUMPIXELS 6
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
 
 void setup() {
-    pixels.begin();
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+  pixels.begin();
 }
 
 void loop() {
-    pixels.clear();
+  pixels.clear();
+  
 
-    for(int i = 0; i < NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-        pixels.show();
-        delay(DELAYVAL);
-    }
+  for(int i=0; i<NUMPIXELS; i++) {
+
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    pixels.show();
+    delay(DELAYVAL);
+  }
+
+
 }
